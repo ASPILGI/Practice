1c958e500f93759943ec9826ae2288d088e43537
이용관 (Lee YongGwan) <84302546+ASPILGI@users.noreply.github.com>
2022-07-12 11:18:47 +0900

    Create k번째수.java

---
+++ Programmers/k번째수.java
@@ -0,0 +1,15 @@
+// x번째 수를 구할때 인덱스에서는 -1을 해줘야한다 그이유는 인덱스는 0부터 시작하기 때문이다.
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i=0; i<commands.length; i++){
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
+        }
+
+        return answer;
+    }
+}
